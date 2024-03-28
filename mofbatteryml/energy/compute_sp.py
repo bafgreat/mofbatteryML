8c6efe78649be92e49bc47195467f3217febe0b1
bafgreat <75862753+bafgreat@users.noreply.github.com>
2024-03-28 11:17:49 +0100

    fixed branch

---
+++ mofbatteryml/energy/compute_sp.py
@@ -0,0 +1,73 @@
+import os
+import shutil
+from mofbatteryml.io import coords_library
+from mofbatteryml.io import filetyper
+import random
+
+
+def compute_xtb_energy(ase_atoms):
+    """
+    Function to compute the xtb energy
+    parameter
+    ----------
+    ase_atoms: ase.Atoms object
+    """
+    base_dir = os.getcwd()
+    random_number = random.uniform(0, 10**8)
+    result_folder = 'tmp_dir_'+str(random_number)
+    if not os.path.exists(result_folder):
+        os.makedirs(result_folder)
+    os.chdir(result_folder)
+    tmp_in = 'tmp_energy.gen'
+    tmp_out = 'tmp_energy.out'
+    coords_library.write_ase_atoms(ase_atoms, tmp_in)
+    os.system(f'xtb --sp --gfn 2 --tblite --spinpol {tmp_in} > {tmp_out}')
+    energy = read_xtb_energy(tmp_out)
+    os.chdir(base_dir)
+    if os.path.exists(result_folder):
+        shutil.rmtree(result_folder)
+    return energy
+
+
+def read_xtb_energy(filename):
+    """
+    Function to read the xtb energy
+    parameter
+    ----------
+    filename: string
+    """
+    contents = filetyper.get_contents(filename)
+    energy = {}
+    for line in contents:
+        if 'TOTAL ENERGY' in line:
+            data = line.split()
+            energy['energy_kcal_mol'] = float(data[3])*627.5095
+        if 'HOMO-LUMO GAP' in line:
+            data = line.split()
+            energy['homo_lumo_ev'] = float(data[3])
+    return energy
+
+
+def compute_energy_of_atom(folder_of_atoms, output_folder):
+    """
+    Function to compute the xtb energy
+    parameter
+    ----------
+    ase_atoms: ase.Atoms object
+    """
+    all_energy = {}
+    json_filename = f'{output_folder}/energy_of_atoms.json'
+    for filename in folder_of_atoms:
+        basename = filename.split('/')[-1].split('_')[0]
+        ase_atoms = coords_library.read_and_return_ase_atoms(filename)
+        energy = compute_xtb_energy(ase_atoms)
+        all_energy[basename] = energy
+        filetyper.append_json(all_energy, json_filename)
+
+
+# def compute_xtb
+
+# folder_of_atoms = glob.glob("../data/metal_atoms/*.xyz")
+# output_folder = '../data/json_files'
+# compute_energy_of_atom(folder_of_atoms, output_folder)
+# compute_xtb_energy("../data/metal_atoms/Al_atom.xyz")
